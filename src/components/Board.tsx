90866673e154631d0347788dec470f4cd3ed5233
naseokju <lukas95@naver.com>
2023-04-21 01:42:18 +0900

    5.13 Task Objects

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,7 +1,8 @@
-import { useRef } from "react";
+import { useForm } from "react-hook-form";
 import { Droppable } from "react-beautiful-dnd";
 import styled from "styled-components";
 import DraggableCard from "./DraggableCard";
+import { ITodo } from "../atoms";
 
 const Wrapper = styled.div`
   width: 300px;
@@ -28,30 +29,43 @@ const Area = styled.div<IAreaProps>`
     padding: 20px;
 `;
 
+const Form = styled.form`
+    width: 100%;
+    input { 
+        width: 100%;
+    }
+`;
+
 interface IAreaProps {
     isDraggingFromThis: boolean;
     isDraggingOver: boolean
 }
 
 interface IBoardProps {
-    toDos: string[];
+    toDos: ITodo[];
     boardId: string;
 }
 
+interface IForm {
+    toDo: string;
+}
+
 function Board({ toDos, boardId }: IBoardProps) {
-    const inputRef = useRef<HTMLInputElement>(null);
-    const onClick = () => {
-        inputRef.current?.focus();
-        setTimeout(() => {
-            inputRef.current?.blur();
-        }, 5000);
-    };
+    const {register, setValue, handleSubmit} = useForm<IForm>();
+    const onValid = (data: IForm) => {
+        console.log(data);
+        setValue("toDo", "");
+    }
 
     return (
         <Wrapper>
             <Title>{boardId}</Title>
-            <input ref={inputRef} placeholder="grab me" />
-            <button onClick={onClick}>click me</button>
+            <Form onSubmit={handleSubmit(onValid)}>
+                <input
+                    {...register("toDo", { required: true })} 
+                    type="text" 
+                    placeholder={`Add task on ${boardId}`} />
+            </Form>
             <Droppable droppableId={boardId}>
                 {(magic, info) => (
                 <Area
@@ -61,7 +75,12 @@ function Board({ toDos, boardId }: IBoardProps) {
                     {...magic.droppableProps}
                 >
                     {toDos.map((toDo, index) => (
-                    <DraggableCard key={toDo} index={index} toDo={toDo} />
+                    <DraggableCard 
+                        key={toDo.id} 
+                        index={index} 
+                        toDoId={toDo.id}
+                        toDoText={toDo.text}
+                    />
                     ))}
                     {magic.placeholder}
                 </Area>
--- src/components/DraggableCard.tsx
+++ src/components/DraggableCard.tsx
@@ -11,15 +11,14 @@ const Card = styled.div<{ isDragging: boolean }>`
 `;
 
 interface IDraggableCardProps {
-    toDo: string;
+    toDoId: number;
+    toDoText: string;
     index: number;
 }
 
-function DraggableCard({ toDo, index }: IDraggableCardProps) {
-    console.log(toDo, "has been rendered");
-
+function DraggableCard({ toDoId, toDoText, index }: IDraggableCardProps) {
     return (
-        <Draggable draggableId={toDo} index={index} key={toDo}>
+        <Draggable draggableId={toDoId + ""} index={index}>
             {(magic, snapshot) => (
                 <Card 
                     isDragging={snapshot.isDragging}
@@ -27,7 +26,7 @@ function DraggableCard({ toDo, index }: IDraggableCardProps) {
                     {...magic.draggableProps}
                     {...magic.dragHandleProps}
                 >
-                    {toDo}
+                    {toDoText}
                 </Card>
             )}
         </Draggable>
